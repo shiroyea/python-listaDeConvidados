977296dae013fcb1a78d4455b59bbe4b03799a44
shiroyea <yuliany.aguey@escola.pr.gov.br>
2025-08-27 08:47:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-#Lista de convidados para a festa
-convidados_da_festa= ['Ana','Diego','Yuliany']
+convidados_da_festa= ['Maria', 'João', 'Ana', 'Carlos', 'Mariana']
 
 status_presenca= {}
 
-#Lista de pessoas que tentaram entrar
-lista_de_chegadas= ['Ana','Diego']
+print('---Verificação da Lista de Convidados')
+
+lista_de_chegadas= ['João', 'Ana', 'Pedro', 'Maria']
+
+for pessoa in lista_de_chegadas:
+    if pessoa in convidados_da_festa:
+        print(f'Olá, {pessoa}! Bem-vindo(a) á festa.')
+        status_presenca[pessoa]= 'Confirmado'
+    
+    else:
+        print(f'Desculpa, {pessoa}. Seu nome não está na lista')
+        status_presenca[pessoa]= 'Não convidado'
+
+    
